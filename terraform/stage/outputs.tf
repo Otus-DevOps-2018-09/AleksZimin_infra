5204e2f5634b9fb48254f51a4afce223812e4296
AleksZimin <realzav@gmail.com>
2019-02-20 19:16:10 +0300

    HW-8. Main task.

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -2,6 +2,10 @@ output "reddit_apps_external_ip" {
   value = "${module.app.reddit_apps_external_ip}"
 }
 
+output "mongo_db_external_ip" {
+  value = "${module.db.db_ext_ip}"
+}
+
 output "reddit_apps_urls" {
   value = "${module.app.reddit_apps_urls}"
 }
